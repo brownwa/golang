8dfd872ba3d82ed010c7d8f20a00eb54acc6361d
Waheed Brown <waheedbrown@google.com>
2020-08-29 13:28:20 -0700

    Imported package from remote module

--- hello/hello.go
+++ hello/hello.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 // Run the main Go code.
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hellow World", "Hello Go"))
 }
